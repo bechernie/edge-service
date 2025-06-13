7ee055b84355a8a984c3f78d12c6355456efa46d
bechernie <bechernie@gmail.com>
2025-06-13 11:13:47 +0200

    oidc login

---
+++ src/main/java/com/github/bechernie/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.github.bechernie.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
